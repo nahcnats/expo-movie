08acaba6d3350f4361a33116d4c5c8cd9ef140ab
nahcnats <nahcnats@gmail.com>
2024-02-08 15:58:22 +0800

    initial

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View
+        // style={styles.container}
+        className='flex-1 justify-center items-center'
+      >
+        <Text className='text-sky-500'>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </GestureHandlerRootView>
+    
   );
 }
 
