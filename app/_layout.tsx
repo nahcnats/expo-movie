3736e4975b89e9117c747a363f62adda45c9b39e
nahcnats <nahcnats@gmail.com>
2024-02-08 16:26:37 +0800

    expo router

---
+++ app/_layout.tsx
@@ -0,0 +1,19 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { View, Text } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+const RootLayoutNav = () => {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+        <View
+            className='flex-1 justify-center items-center'
+        >
+            <Text className='text-sky-500'>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    </GestureHandlerRootView>
+  );
+};
+
+export default RootLayoutNav;
