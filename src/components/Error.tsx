5f8b78d13f3ce04f244bae7f280d238c7a48cae5
nahcnats <nahcnats@gmail.com>
2024-02-11 19:22:28 +0800

    error go back

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+import { MainNavigationParams } from '../navigators/MainNavigation';
 
 const Error = () => {
+    const navigation = useNavigation<NativeStackNavigationProp<MainNavigationParams>>();
+    
     return (
         <View className='flex-1 justify-center items-start'>
-            <Text className='text-base text-red-500'>Error</Text>
+            <Text className='text-base text-red-500 font-bold'>Error</Text>
+            <Text className='text-base text-tertiary font-semibold' onPress={() => navigation.goBack()}>Go back</Text>
         </View>
     );
 };
